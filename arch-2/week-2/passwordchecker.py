516ce5a5c87abdd79769171c7324c483554e8602
Jurian Andriessen <jurian@Jurians-MacBook-Pro.local>
2022-10-11 18:07:06 +0200

    chore(feat): Added excercise 3 of week 6

--- arch-2/week-2/passwordchecker.py
+++ arch-2/week-2/passwordchecker.py
@@ -10,3 +10,42 @@ Implement a Python program that asks the password of the user and checks if it i
 
 * Use sets and set operations to solve this problem.
 '''
+
+def passwordCheck():
+    validSymbols = {'*', '@', '!', '?'}
+    tries = 0
+
+    while tries < 3:
+        password = input('Enter a password: ')
+        password = password.replace(' ', '')
+
+        if 8 <= len(password) <= 20:
+            containsDigit = False
+            containsUppercase = False
+            containsLowercase = False
+            containsSymbol = False
+
+            for char in password:
+                if char.isdigit():
+                    containsDigit = True
+                elif char.isalpha():
+                    containsUppercase = True
+                elif char.islower():
+                    containsLowercase = True
+                elif char in validSymbols:
+                    containsSymbol = True
+                else:
+                    print('You used wrong characters')
+                    tries += 1
+                    break
+            if containsDigit and containsUppercase and containsLowercase and containsSymbol:
+                return 'Good password'
+            else:
+                print('a valid password must be a combination of digits, uppercase and lowercase letters and one symbol (* @ ! ?)')
+        else:
+            print('Your password needs to be 8-20 characters')
+            tries += 1
+
+
+if __name__ == '__main__':
+    print(passwordCheck())
