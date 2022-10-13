b367c6f28a12a6f75f7f2c434e2095b63efd3f7c
Jurian Andriessen <jurian@Jurians-MacBook-Pro.local>
2022-10-13 18:03:21 +0200

    chore(feat): Finished excercise 3 of week 6

--- arch-2/week-2/passwordchecker.py
+++ arch-2/week-2/passwordchecker.py
@@ -1,51 +1,34 @@
-'''
-In an application a valid password must be a combination of digits, uppercase and lowercase letters
-and only four symbols * @ ! ? .
-
-The length of the password must not be less than 8 characters
-and must not be more than 20 characters.
-In case the password is not valid, the user can try maximum three times until it is validated.
-
-Implement a Python program that asks the password of the user and checks if it is a valid password.
-
-* Use sets and set operations to solve this problem.
-'''
-
 def passwordCheck():
-    validSymbols = {'*', '@', '!', '?'}
+    upper_set = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    lower_set = set("abcdefghijklmnopqrstuvwxyz")
+    digit_set = {"1234567890"}
+    symbol_set = set("*@!?")
+
     tries = 0
 
     while tries < 3:
-        password = input('Enter a password: ')
+        password = input('')
         password = password.replace(' ', '')
 
-        if 8 <= len(password) <= 20:
-            containsDigit = False
-            containsUppercase = False
-            containsLowercase = False
-            containsSymbol = False
+        pass_set = set(password)
+        flag = 0
 
-            for char in password:
-                if char.isdigit():
-                    containsDigit = True
-                elif char.isalpha():
-                    containsUppercase = True
-                elif char.islower():
-                    containsLowercase = True
-                elif char in validSymbols:
-                    containsSymbol = True
-                else:
-                    print('You used wrong characters')
-                    tries += 1
-                    break
-            if containsDigit and containsUppercase and containsLowercase and containsSymbol:
-                return 'Good password'
+        if 8 <= len(password) <= 20:
+            if bool(upper_set.intersection(pass_set)):
+                if bool(lower_set.intersection(pass_set)):
+                    if bool(digit_set.intersection(pass_set)):
+                        if bool(symbol_set.intersection(pass_set)):
+                            flag = 1
+            if flag:
+                return 'Valid'
             else:
-                print('a valid password must be a combination of digits, uppercase and lowercase letters and one symbol (* @ ! ?)')
+                tries += 1
+                print('Invalid')
         else:
-            print('Your password needs to be 8-20 characters')
             tries += 1
 
+        if tries == 3:
+            return 'Invalid'
 
 if __name__ == '__main__':
     print(passwordCheck())
