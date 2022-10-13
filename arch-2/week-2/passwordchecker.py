01bdfd1fcf120a7e7e1991b83da938227be5ae38
Jurian Andriessen <jurian@Jurians-MacBook-Pro.local>
2022-10-13 18:05:47 +0200

    chore(feat): Finished excercise 3 of week 6

--- arch-2/week-2/passwordchecker.py
+++ arch-2/week-2/passwordchecker.py
@@ -1,8 +1,9 @@
 def passwordCheck():
     upper_set = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     lower_set = set("abcdefghijklmnopqrstuvwxyz")
-    digit_set = {"1234567890"}
+    digit_set = set("1234567890")
     symbol_set = set("*@!?")
+    print(upper_set, lower_set, digit_set, symbol_set)
 
     tries = 0
 
@@ -10,25 +11,36 @@ def passwordCheck():
         password = input('')
         password = password.replace(' ', '')
 
-        pass_set = set(password)
-        flag = 0
+        containsUpper, containsLower, containsDigit, containsSymbol = False, False, False, False
 
         if 8 <= len(password) <= 20:
-            if bool(upper_set.intersection(pass_set)):
-                if bool(lower_set.intersection(pass_set)):
-                    if bool(digit_set.intersection(pass_set)):
-                        if bool(symbol_set.intersection(pass_set)):
-                            flag = 1
-            if flag:
+            for char in password:
+                char = set(char)
+
+                if bool(upper_set.intersection(char)):
+                    containsUpper = True
+                elif bool(lower_set.intersection(char)):
+                    containsLower = True
+                elif bool(digit_set.intersection(char)):
+                    containsDigit = True
+                elif bool(symbol_set.intersection(char)):
+                    containsSymbol = True
+                else:
+                    containsUpper, containsLower, containsDigit, containsSymbol = False, False, False, False
+
+            if containsUpper and containsLower and containsDigit and containsSymbol:
                 return 'Valid'
             else:
+                print(containsUpper, containsLower, containsDigit, containsSymbol)
                 tries += 1
                 print('Invalid')
         else:
             tries += 1
 
         if tries == 3:
+            print('paashaas')
             return 'Invalid'
+
 
 if __name__ == '__main__':
     print(passwordCheck())
