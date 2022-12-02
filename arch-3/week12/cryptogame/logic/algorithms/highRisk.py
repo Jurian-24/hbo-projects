b31c8cff41ce2db8591f36194026ecabdb50649b
Jurian Andriessen <jurian@Jurians-MacBook-Pro.local>
2022-12-02 23:06:01 +0100

    chore(feat): basics for the game

---
+++ arch-3/week12/cryptogame/logic/algorithms/highRisk.py
@@ -0,0 +1,6 @@
+class HighRisk:
+    def __init__(self, coin: dict):
+        self.test = 'Hello world'
+
+    def test(self):
+        print(self.test)
--- arch-3/week12/cryptogame/main.py
+++ arch-3/week12/cryptogame/main.py
@@ -1,4 +1,25 @@
 import requests
 import json
+import time
 from dotenv import load_dotenv
+
+# import algorithms
+
+from logic.algorithms.highRisk import highRisk
+
+
+def getCoins() -> dict:
+    response = requests.get('https://api.basecampcrypto.nl/v1/coin?key=qx95gdbja26sZa5P')
+
+    return json.loads(response.text)
+
+def getCoinInfo(symbol) -> dict:
+    response = requests.get(f'https://api.basecampcrypto.nl/v1/coin/{symbol}/history?key=qx95gdbja26sZa5P')
+
+    return json.loads(response.text)
+
+if __name__ == '__main__':
+    while True:
+        time.sleep(1)
+        print(highRisk.test())
 
