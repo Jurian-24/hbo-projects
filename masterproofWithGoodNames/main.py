3477786fadd61e18960ced0307e836ee2b204bae
Jurian Andriessen <jurian@Jurians-MacBook-Pro.local>
2023-01-10 17:22:32 +0100

    chore(feat): Finished the database seeder

--- masterproofWithGoodNames/main.py
+++ masterproofWithGoodNames/main.py
@@ -7,6 +7,7 @@ from ClassClimber import Climber
 from ClassMountain import Mountain
 from ClassExpedition import Expedition
 
+
 def read_json_file():
     fhand = open('expeditions.json', encoding="utf8") # fhand = file handeling
 
@@ -15,9 +16,11 @@ def read_json_file():
 
     return expeditions_data_dict
 
+
 def configure_database(data):
     mountains = [expedition['mountain'] for expedition in data]
     set_mountains(mountains, data)
+
 
 def set_mountains(mountains: list, data):
     conn = sqlite3.connect(os.path.join(sys.path[0], 'climbersapp.db'))
@@ -44,7 +47,6 @@ def set_mountains(mountains: list, data):
         set_expedition(data, mountain['name'], executedQuery.lastrowid, conn)
 
 
-
 def set_expedition(data, mountainName, mountainId, conn):
     for expedition in data:
         if expedition['mountain']['name'] == mountainName:
@@ -68,7 +70,38 @@ def set_expedition(data, mountainName, mountainId, conn):
             conn.execute(query, values)
             conn.commit()
 
+            set_climbers(conn, data, expedition)
 
+
+def set_climbers(conn, data, expedition):
+    for climber in expedition['climbers']:
+        query = f"""
+            INSERT OR IGNORE INTO climbers (id, first_name, last_name, nationality, date_of_birth)
+            SELECT ?, ?, ?, ?, ?
+            WHERE NOT EXISTS (SELECT 1 FROM climbers WHERE id = {expedition['id']})
+        """
+
+        values = [
+            climber['id'],
+            climber['first_name'],
+            climber['last_name'],
+            climber['nationality'],
+            climber['date_of_birth']
+        ]
+
+        conn.execute(query, values)
+        set_expedition_climber(conn, climber['id'], expedition['id'])
+
+
+def set_expedition_climber(conn, climberId, expeditionId):
+    query = """
+        INSERT INTO expedition_climbers (climber_id, expedition_id)
+        SELECT ?, ?
+        WHERE NOT EXISTS (SELECT 1 FROM expedition_climbers WHERE climber_id = ? AND expedition_id = ?)
+    """
+
+    conn.execute(query, [climberId, expeditionId, climberId, expeditionId])
+    conn.commit()
 
 
 def truncate_table():
@@ -76,7 +109,10 @@ def truncate_table():
     conn.set_trace_callback(print)
     cursor = conn.cursor()
 
+    conn.execute("DELETE FROM climbers")
+    conn.execute("DELETE FROM expedition_climbers")
     conn.execute("DELETE FROM expeditions")
+    conn.execute("DELETE FROM mountains")
     conn.commit()
     conn.close()
 
