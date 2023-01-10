917e619025d2055667b1a983a72f8c5911f96fb5
Jurian Andriessen <jurian@Jurians-MacBook-Pro.local>
2023-01-10 16:58:31 +0100

    chore(feat): Added the Expedition class

--- masterproofWithGoodNames/ClassExpedition.py
+++ masterproofWithGoodNames/ClassExpedition.py
@@ -1,7 +1,41 @@
+from datetime import datetime
+
+from ClassClimber import Climber
+from ClassMountain import Mountain
+
 class Expedition:
 
-    def __init__(self) -> None:
+    def __init__(self, id: int, name: str, mountain_id: int, start: str, date: datetime, country: str, duration: str, success: bool) -> None:
+        self.id = id
+        self.name = name
+        self.mountain_id = mountain_id
+        self.start = start
+        self.date = date
+        self.country = country
+        self.duration = duration
+        self.success = success
+
+    def add_climber(climber: Climber):
         pass
+
+    def get_climbers() -> list:
+        pass
+
+    def get_mountain() -> Mountain:
+        pass
+
+    def convert_date(to_format: str) -> str:
+        pass
+
+    def convert_duration(to_format: str) -> str:
+        pass
+
+    def get_duration() -> str:
+        pass
+
+    def get_climbers() -> dict:
+        pass
+
 
     # Representation method
     # This will format the output in the correct order
